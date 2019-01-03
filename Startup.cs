b8cc6fc8308e086a5549a3b8a162642a33a2bca9
Jackal Lee <jackcakal@gmail.com>
2019-01-03 15:08:34 +0800

    Setting up connectionStrings in appsettings.json

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace Crr
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
